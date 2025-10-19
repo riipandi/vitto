52447ce49b76c739cfd83e247affe603977f4986
Aris Ripandi <riipandi@gmail.com>
2025-10-20 05:25:19 +0700

    feat: add vitto starter

---
+++ starter/vite.config.ts
@@ -0,0 +1,21 @@
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import vitto from 'vitto'
+
+export default defineConfig({
+  plugins: [
+    vitto({
+      minify: process.env.NODE_ENV === 'production',
+      enableSearchIndex: true,
+      outputStrategy: 'html',
+      metadata: {
+        siteName: 'Vitto',
+        title: 'Vitto - Static Site Generator Powered by Vite & Vento',
+        description: `A minimal static site generator built with Vite and the Vento templating engine.`,
+        keywords: ['vento', 'ssg', 'vite', 'plugin', 'generator', 'static', 'website', 'jamstack'],
+        author: 'Your Name',
+      },
+    }),
+    tailwindcss(),
+  ],
+})
